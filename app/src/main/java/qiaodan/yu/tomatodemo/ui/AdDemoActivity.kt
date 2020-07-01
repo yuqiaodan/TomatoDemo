94bd13edc4cd7b21a4c4210d8fd7065a934bf20e
qiaodan <2031015269@qq.com>
2020-07-01 15:08:58 +0800

    整理代码-2

--- app/src/main/java/qiaodan/yu/tomatodemo/ui/AdDemoActivity.kt
+++ app/src/main/java/qiaodan/yu/tomatodemo/ui/AdDemoActivity.kt
@@ -88,23 +88,19 @@ class AdDemoActivity : AppCompatActivity() {
         (adView.headlineView as TextView).text = nativeAd.headline
 
         adView.mediaView.setMediaContent(nativeAd.mediaContent)
-
-        // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
-        // check before trying to display them.
+        
         if (nativeAd.body == null) {
             adView.bodyView.visibility = View.INVISIBLE
         } else {
             adView.bodyView.visibility = View.VISIBLE
             (adView.bodyView as TextView).text = nativeAd.body
         }
-
         if (nativeAd.callToAction == null) {
             adView.callToActionView.visibility = View.INVISIBLE
         } else {
             adView.callToActionView.visibility = View.VISIBLE
             (adView.callToActionView as Button).text = nativeAd.callToAction
         }
-
         if (nativeAd.icon == null) {
             adView.iconView.visibility = View.GONE
         } else {
@@ -113,21 +109,18 @@ class AdDemoActivity : AppCompatActivity() {
             )
             adView.iconView.visibility = View.VISIBLE
         }
-
         if (nativeAd.price == null) {
             adView.priceView.visibility = View.INVISIBLE
         } else {
             adView.priceView.visibility = View.VISIBLE
             (adView.priceView as TextView).text = nativeAd.price
         }
-
         if (nativeAd.store == null) {
             adView.storeView.visibility = View.INVISIBLE
         } else {
             adView.storeView.visibility = View.VISIBLE
             (adView.storeView as TextView).text = nativeAd.store
         }
-
         if (nativeAd.starRating == null) {
             adView.starRatingView.visibility = View.INVISIBLE
         } else {
